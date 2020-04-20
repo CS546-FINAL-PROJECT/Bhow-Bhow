ba20b7158f9be9ba1391f7173430ffa163db3c99
ppate78 <32399008+ppate78@users.noreply.github.com>
2020-04-20 11:51:32 -0400

    Add files via upload

--- Pet/routes/back_login.js
+++ Pet/routes/back_login.js
@@ -1,2 +1,15 @@
-/*
-*/
+var express    = require("express");
+var login = require('./routes/loginroutes');
+var bodyParser = require('body-parser');
+var app = express();
+
+var router = express.Router();
+// test route
+router.get('/', function(req, res) {
+    res.json({ message: 'Welcome to the login' });
+});
+//route to handle user registration
+router.post('/register',login.register);
+router.post('/login',login.login)
+app.use('/api', router);
+app.listen(3000);
