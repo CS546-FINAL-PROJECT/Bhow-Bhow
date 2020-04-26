f5a9b5ce8563475165cc2cfaaf1b549bf2815f87
Mohammed Mohiuddin <maaazzz.mohd@gmail.com>
2020-04-26 23:05:46 +0530

    Code Inserted

--- Pet/models/advertiser.js
+++ Pet/models/advertiser.js
@@ -1,8 +1,10 @@
 var mongoose = require("mongoose");
 
 var advertiserSchema = new mongoose.Schema({
+	        advertiser__id:String;
 		advertiser_firstName:String,
 		advertiser_lastName:String,
+	        advertiser_email:String,
 		advertiser_userPassword:String,
 		advertiser_cellphone:String,
 		advertiser_address:String,
