8d4333105afc6179302d489f0c6b761e9abf3b54
Mohammed Mohiuddin <maaazzz.mohd@gmail.com>
2020-04-26 06:29:01 +0530

    Wrote Code

--- Pet/routes/index.js
+++ Pet/routes/index.js
@@ -1,2 +1,24 @@
-/*
-*/
+const advertisersRoutes = require('./advertisers');
+const commentsRoutes = require('./comments');
+const dogsRoutes = require('./dogs');
+const loginRoutes = require('./login');
+const usersRoutes = require('./users');
+const path = require('path');
+
+const constructorMethod = (app) => {
+	app.use('/advertisers', advertisersRoutes);
+	app.use('/comments', commentsRoutes);
+  app.use('/dogs', dogsRoutes);
+	app.use('/login', loginRoutes);
+  app.use('/users', userRoutes);
+  
+	app.get('/about', (req, res) => {
+		res.sendFile(path.resolve('static/about.html'));
+	});
+
+	app.use('*', (req, res) => {
+		res.redirect('/posts');
+	});
+};
+
+module.exports = constructorMethod;
