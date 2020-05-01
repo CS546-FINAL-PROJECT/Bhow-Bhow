29b52976f5ee487a8d2d2149eb9f3238eb39b08f
ppate78 <32399008+ppate78@users.noreply.github.com>
2020-05-01 11:52:58 -0400

    Update login.js

--- Pet/routes/login.js
+++ Pet/routes/login.js
@@ -5,11 +5,82 @@ var app = express();
 
 var router = express.Router();
 // test route
-router.get('/', function(req, res) {
-    res.json({ message: 'Welcome to the login' });
+async function isAuth(req){
+    try{
+        var user=await userData.findUserBySID(req.cookies.AuthCookie);
+
+        if (req.cookies.AuthCookie!==undefined && req.cookies.AuthCookie==user.sessionID && user.sessionID!==undefined)
+        {
+
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    catch(e)
+    {
+        return false;
+    }
+
+}
+
+router.get("/", async(req, res) =>{
+    if(await isAuth(req)===true)    {
+        res.redirect("/index");
+    }
+    else {
+        res.render("Users_login");
+    }
 });
-//route to handle user registration
-router.post('/register',login.register);
-router.post('/login',login.login)
+
+router.post("/login", async(req, res) =>{
+    const name=req.body.username;
+    const pw=req.body.password;
+
+    var userExists=false;
+    var correctPW=false;
+    var user;
+
+    try{
+        user=await userData.findUser(name);
+        userExists=true;
+    }
+    catch(e){
+        userExists=false;
+    } 
+
+    try{
+        if(await userData.checkPW(name,pw));
+            correctPW=true;
+    }
+    catch(e){
+        correctPW=false;
+    } 
+
+    if(userExists && correctPW) {
+        let sid=uuid();
+
+        user.sessionID=sid;
+
+        res.cookie("AuthCookie", sid);
+        res.redirect("/index");
+    }
+    else
+    {
+        res.render("User_login", {error: "Invalid Username/Password"});
+    }
+});
+
+router.get("/logout", async(req,res) =>
+{
+    res.cookie("AuthCookie", "", {expires:new Date()});
+    res.clearCookie("AuthCookie");
+    res.render("User_login", {title:"You have been logged out"});
+});
+
+router.post('/Users_register',login.register);
+router.post('/Users_login',login.login)
 app.use('/api', router);
 app.listen(3000);
