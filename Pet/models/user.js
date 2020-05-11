e62a0ebec576c70e34411e11f0a1d40f49cf7f07
J-Vincent <41374606+J-Vincent@users.noreply.github.com>
2020-05-11 00:33:29 -0400

    Update user.js

--- Pet/models/user.js
+++ Pet/models/user.js
@@ -1,13 +1,26 @@
 var mongoose = require("mongoose");
+var passportLocalMongoose = require("passport-local-mongoose"); 
 
-var userSchema = new mongoose.Schema({
-		username:String,
-		password:String,
-		email:String,
-		gender:String,
-		address:String,
-		age:String,
-		favoriteList:[]
-	});
 
-module.exports = mongoose.model("User",userSchema);
+var UserSchema = new mongoose.Schema({
+	username:String,
+	password:String,
+	user_email:String,
+	user_gender:String,
+	user_address:String,
+	user_age:String,
+	favoriteList:[]
+});
+
+UserSchema.methods.authenticate = function(password) {      
+	return this.password === this.hashPassword(password);
+  };
+// UserSchema.plugin(uniqueValidator);
+// UserSchema.methods.validPassword = function(password) {
+// 	return bcrypt.compareSync(password, this.passwordHash);
+//   };
+UserSchema.plugin(passportLocalMongoose); 
+module.exports = mongoose.model("User",UserSchema);
+
+
+
