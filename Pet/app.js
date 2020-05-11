9175cf5849bcd7a0d5390332e695992ed4ff9e5a
J-Vincent <41374606+J-Vincent@users.noreply.github.com>
2020-05-11 00:29:59 -0400

    Update app.js

--- Pet/app.js
+++ Pet/app.js
@@ -1,46 +1,84 @@
 const express = require('express');
 const session = require('express-session');
-const exphbs = require('express-handlebars');
+var ejs = require('ejs')
 const bodyParser = require('body-parser');
 const mongoose=require("mongoose");
 const path = require('path');
-
+const passport = require('passport');
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
+const Advertiser = require("./models/advertiser")
 
 const app = express();
 
 const configRoutes = require('./routes');
-const handlebarsInstance = exphbs.create({
-	defaultLayout: 'main'
-});
+
 
 var url = process.env.DATABASEURL || "mongodb://localhost/dogAdoption";
-mongoose.connect(url);
+mongoose.connect(url,{ useNewUrlParser: true , useUnifiedTopology: true });
+app.use(express.static(__dirname + '/views'));
 
-app.use(express.static(__dirname + '/public'));
-app.set('views',path.join(__dirname + '/views'));
-app.set('view engine','handlebars');
-app.engine('handlebars', handlebarsInstance.engine);
+app.engine('html', ejs.__express);
+app.set('view engine', 'html');
+
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(session({
-	name: 'AuthCookie',
-	secret: 'some secret string!',
-	resave: false,
-	saveUninitialized: true
-  }))
-  
-  app.use((req, res, next) =>{
-	var text;
-	if(req.session.user !== "" || req.session.user !== undefined || req.session.user.length == 0)
-	{
-	  text = "(Non-Authenticated User)"
-	}
-	else text = "(Authenticated User)"
-	console.log(new Date().toUTCString(), req.method, req.originalUrl, text);
-	next();
-  })
+//  session 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+// Stragety for user
+
+passport.use('User',new LocalStrategy({
+	usernameField: 'username',
+	passwordField: 'password' // this is the virtual field on the model
+},	
+	function(username, password, done) {
+		User.findOne({ username: username }, function (err, user) {
+		  if (err) { return done(err); }
+		  if (!user) {
+			return done(null, false, { message: 'Incorrect username.' });
+		  }
+		  if (!user.authenticate(password)) {
+			return done(null, false, { message: 'Incorrect password.' });
+		  }
+		  return done(null, user);
+		});
+}));
+// Stragety for advertiser
+
+passport.use('Adevrtiser',new LocalStrategy({
+	usernameField: 'username',
+	passwordField: 'password' // this is the virtual field on the model
+},	
+	function(username, password, done) {
+		Advertiser.findOne({ username: username }, function (err, user) {
+		  if (err) { return done(err); }
+		  if (!user) {
+			return done(null, false, { message: 'Incorrect username.' });
+		  }
+		//   console.log(user);
+		  if (!user.authenticate(password)) {
+			return done(null, false, { message: 'Incorrect password.' });
+		  }
+		  return done(null, user);
+		});
+}));
+
+
+app.use(require("express-session")({    
+	secret:"Hello World, this is a session",    
+	resave: false,    
+	saveUninitialized: false
+}));
+
 
 configRoutes(app);
+
 
 app.listen(3000, () => {
 	console.log("We've now got a server!");
