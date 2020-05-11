02f6ffa055dfddf9363770259d579e7d16018212
J-Vincent <41374606+J-Vincent@users.noreply.github.com>
2020-05-11 00:33:48 -0400

    Update advertiser.js

--- Pet/models/advertiser.js
+++ Pet/models/advertiser.js
@@ -1,14 +1,18 @@
 var mongoose = require("mongoose");
+var passportLocalMongoose = require("passport-local-mongoose"); 
 
 var advertiserSchema = new mongoose.Schema({
-	        advertiser__id:String,
+		username:String,
 		advertiser_firstName:String,
 		advertiser_lastName:String,
-	        advertiser_email:String,
+	    advertiser_email:String,
 		advertiser_userPassword:String,
 		advertiser_cellphone:String,
 		advertiser_address:String,
 		advertiser_dogList:String
 	});
-
+advertiserSchema.methods.authenticate = function(password) {      
+		return this.password === this.hashPassword(password);
+	  };
+advertiserSchema.plugin(passportLocalMongoose); 
 module.exports = mongoose.model("Advertiser",advertiserSchema);
