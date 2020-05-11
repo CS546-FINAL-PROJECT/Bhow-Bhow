aabe5f3df7d4e49a1728eb30541d607fc03695db
J-Vincent <41374606+J-Vincent@users.noreply.github.com>
2020-05-11 17:26:01 -0400

    Update seed.js

--- Pet/seed.js
+++ Pet/seed.js
@@ -1,18 +1,43 @@
-var mongoose = require("mongoose");
+
 var Dog = require("./models/dog");
 var Comment = require("./models/comment");
+var Dog = require("./models/dog")
 
 var data = [{
 	dog_breed:"combai",
 	dog_name:"Yankee",
-	dog_age:"Twelve",
+	dog_age:"12 monthes",
 	dog_address:"183 Hutton St",
 	dog_description:"A very cute and faithful dog",
-	dog_vaccine:[A,c,v],
-	dog_picture:"String",
+	dog_vaccine:["Distemper","Parvovirus","Parainfluenza"],
+	dog_picture:"https://www.dogbreedinfo.com/images31/CombaiBoarhoundDogRareBreedIndiaTiger2YearsOldSideView.jpg",
 	dog_postdate:"21-April-2020",
-	dog_color:"Blue",
+	dog_color:"Yellow",
 	dog_size:"12 ft",
+	dog_gender:"Male"
+},{
+	dog_breed:"Alaskan Malamute",
+	dog_name:"Kiki",
+	dog_age:"4 monthes",
+	dog_address:"52 11th St,Hoboken",
+	dog_description:"An affectionate, loyal, and playful but dignified dog",
+	dog_vaccine:["Parvovirus","Parainfluenza"],
+	dog_picture:"https://vetstreet.brightspotcdn.com/dims4/default/e1bbd79/2147483647/crop/0x0%2B0%2B0/resize/645x380/quality/90/?url=https%3A%2F%2Fvetstreet-brightspot.s3.amazonaws.com%2F82%2F071660a40111e087a80050568d634f%2Ffile%2FAlaskan-Malamute-3-605mk062311.jpg",
+	dog_postdate:"20-April-2020",
+	dog_color:"White and Pink",
+	dog_size:"12 ft",
+	dog_gender:"Male"
+},{
+	dog_breed:"American Staffordshire Terrier ",
+	dog_name:"Yankee",
+	dog_age:"16 monthes",
+	dog_address:"183 Hutton St",
+	dog_description:"Very smart, confident, good-natured companions. Their courage is proverbial",
+	dog_vaccine:["Distemper","Parvovirus"],
+	dog_picture:"https://vetstreet.brightspotcdn.com/dims4/default/94fc9d8/2147483647/thumbnail/645x380/quality/90/?url=https%3A%2F%2Fvetstreet-brightspot.s3.amazonaws.com%2Fc5%2Fe57d80a71811e0a0d50050568d634f%2Ffile%2FAmerican-Staffordshire-Terrier-3-645mk070411.jpg",
+	dog_postdate:"25-April-2020",
+	dog_color:"Yellow",
+	dog_size:"14 ft",
 	dog_gender:"Male"
 }];
 
@@ -47,4 +72,5 @@ function seedDB(){
     })
 });
 }
+seedDB();
 module.exports = seedDB;
